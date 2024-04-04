b0d00a566199e09e5d027625007f62d476ba7606
mohamedshetewy1 <msheto26@gmail.com>
2024-04-04 17:11:00 +0200

    add calender

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: NavDoctor(),
+      home: SplashScreen(),
     );
   }
 }
